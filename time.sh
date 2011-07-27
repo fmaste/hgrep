8a12df4ff11dc31664bc617972afb3577f73bd3a
fmaste <fmaste@users.noreply.github.com>
2011-07-27 19:27:32 -0300

    No column is printed

--- time.sh
+++ time.sh
@@ -4,7 +4,7 @@
 echo "\n"
 echo "--------------------------------------------------------------------------------"
 echo "\n"
-time grep lala /usr/share/dict/words -o -n -u
+time grep lala /usr/share/dict/words -o -n
 echo "\n"
 echo "--------------------------------------------------------------------------------"
 echo "\n"
