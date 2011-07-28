20f20b08b864c619841d4ddbe4937cd3906fc7bc
fmaste <fmaste@users.noreply.github.com>
2011-07-28 11:42:01 -0300

    Use POSIX time output

--- time.sh
+++ time.sh
@@ -4,9 +4,9 @@
 echo
 echo "--------------------------------------------------------------------------------"
 echo
-time grep lala /usr/share/dict/words -o -n
+time -p grep lala /usr/share/dict/words -o -n
 echo
 echo "--------------------------------------------------------------------------------"
 echo
-time ./hrgrep lala /usr/share/dict/words +RTS -K100000000
+time -p ./hrgrep lala /usr/share/dict/words +RTS -K100000000
 
