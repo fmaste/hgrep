e3b7c11a3090c4f8ea2d9f3aed5736d97c30a6d6
fmaste <fmaste@users.noreply.github.com>
2011-07-28 21:15:31 -0300

    Remove before build

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm hgrep
 ghc Main.hs -o hgrep -O2 -fforce-recomp -threaded -package mtl -package bytestring -package parallel
 
--- maketicky.sh
+++ maketicky.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm hgrep
 ghc Main.hs -o hgrep -O2 -fforce-recomp -package mtl -package bytestring -package parallel -ticky
 
