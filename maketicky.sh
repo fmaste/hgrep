369f47f2e26f3a9f13f8487fa8708564df070dea
fmaste <fmaste@users.noreply.github.com>
2011-07-29 09:33:55 -0300

    Also threads

--- maketicky.sh
+++ maketicky.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm hgrep
-ghc Main.hs -o hgrep -O2 -fforce-recomp -package mtl -package bytestring -package parallel -ticky
+ghc Main.hs -o hgrep -O2 -fforce-recomp -threaded -package mtl -package bytestring -package parallel -ticky
 
