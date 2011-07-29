1c4c01a2b3e625e1a5e9715e1eddabb85a306c60
fmaste <fmaste@users.noreply.github.com>
2011-07-29 01:50:41 -0300

    Threaded

--- makeprof.sh
+++ makeprof.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm hgrep
-ghc Main.hs -o hgrep -O2 -fforce-recomp -package mtl -package bytestring -package parallel -prof -auto-all -caf-all
+ghc Main.hs -o hgrep -O2 -fforce-recomp -threaded -package mtl -package bytestring -package parallel -prof -auto-all -caf-all
 
