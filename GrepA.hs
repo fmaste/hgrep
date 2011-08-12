a68150b5687130f76b8aebf73124bca84f60df68
fmaste <fmaste@users.noreply.github.com>
2011-08-12 18:10:53 -0300

    Playing with Arrows

--- GrepA.hs
+++ GrepA.hs
@@ -14,23 +14,54 @@ type Stream a = [a]
 newtype GrepA b c = GrepA {runGrepA :: (Stream b) -> (Stream c)}
 
 instance Category GrepA where
+	
+	-- id :: cat a a
 	id = GrepA id
+
+	-- (.) :: cat b c -> cat a b -> cat a c
 	(GrepA f) . (GrepA g) = GrepA (f . g)
 
 instance Arrow GrepA where
-	arr f = GrepA (map f)
-	first (GrepA f) = GrepA $ \bds -> let (bs, ds) = unzip bds in zip (f bs) ds
-	second (GrepA f) = GrepA $ \dbs -> let (ds, bs) = unzip dbs in zip ds (f bs)
-	(GrepA f) *** (GrepA g) = GrepA $ \bbs -> let (bs, bs') = unzip bbs in zip (f bs) (g bs')
-	(GrepA f) &&& (GrepA g) = GrepA $ \bs -> zip (f bs) (g bs)
 
+	-- arr :: (b -> c) -> a b c
+	arr f = GrepA (map f)
+
+	-- first :: a b c -> a (b, d) (c, d)
+	first (GrepA f) = GrepA $ \bds -> let (bs, ds) = unzip bds in zip (f bs) ds
+	-- Can also be defined as: 
+	-- first (GrepA f) = GrepA (unzip >>> first f >>> uncurry zip)
+
+	-- second :: a b c -> a (d, b) (d, c)
+	second (GrepA f) = GrepA $ \dbs -> let (ds, bs) = unzip dbs in zip ds (f bs)
+
+	-- TODO: Make concurrent, use par
+	-- (***) :: a b c -> a b' c' -> a (b, b') (c, c')
+	(GrepA f) *** (GrepA g) = GrepA $ \bbs -> let (bs, bs') = unzip bbs in zip (f bs) (g bs')
+	-- Can also be defined as:
+	-- (GrepA f) *** (GrepA g) = GrepA $ first f >>> second g
+
+	-- TODO: Make concurrent, use par
+	-- (&&&) :: a b c -> a b c' -> a b (c, c')
+	(GrepA f) &&& (GrepA g) = GrepA $ \bs -> zip (f bs) (g bs)
+	-- Can also be defined as: 
+	-- (GrepA f) &&& (GrepA g) = GrepA $ f &&& g >>> uncurry zip
+
+{-
 instance ArrowZero GrepA where
 	zeroArrow = GrepA $ \_ -> []
 
-{-
 instance ArrowPlus GrepA where
 	(GrepA f) <+> (GrepA g) = GrepA $ \bs -> 
 -}
+
+instance ArrowLoop GrepA where
+	-- loop :: a (b, d) (c, d) -> a b c 
+	-- loop :: arrow (input, feedback) (output, feedback) -> arrow input output
+	loop (GrepA f) = GrepA $ \as -> let (bs, cs) = unzip (f (zip as (stream cs))) in bs
+		where stream ~(x:xs) = x:stream xs
+
+delay :: b -> GrepA b b
+delay b = GrepA (b:)
 
 newtype Automaton b c = Automaton (b -> (c, Automaton b c))
 
