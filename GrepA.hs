e663289b65dc640668db539d06fd2661d515d441
fmaste <fmaste@users.noreply.github.com>
2011-08-17 17:40:33 -0300

    Simplify

--- GrepA.hs
+++ GrepA.hs
@@ -55,7 +55,7 @@ instance Category Stream where
         -- (.) :: cat d c -> cat b d -> cat b c
 	(Put c s) . stream = put c (s . stream)
 	(Get f) . (Put c s) = f c . s
-	stream . (Get f) = get $ \b -> stream . f b
+	(Get f) . (Get g) = get $ \b -> (get f) . (g b)
 
 instance Arrow GrepA where
 
